e8da6a8b592741b83610945977dcf3a60447e6ce
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-11-11 13:19:33 -0500

    add interactive TUI for environment variable selection

--- main.go
+++ main.go
@@ -4,14 +4,115 @@ import (
 	"fmt"
 	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/joho/godotenv"
 )
+
+type model struct {
+	variables []string
+	cursor    int
+	choices   []string
+	selected  map[int]struct{}
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("No local .env found")
 	}
-	envList := os.Environ()
-	fmt.Println("Environment Variables Found: ", envList)
+
+	var envList []string = os.Environ()
+	if os.Getenv("DEBUG") != "" {
+		printList(envList)
+	}
+
+	p := tea.NewProgram(initialModel(envList))
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+}
+
+func printList(list []string) {
+	for count := 0; count < len(list); count++ {
+		fmt.Printf("Environment Variable #%v: %v\n", count, list[count])
+	}
+	fmt.Println("Total Number of Environment Variables", len(list))
+}
+
+func initialModel(prop []string) model {
+	return model{
+		variables: prop,
+		choices:   prop,
+		selected:  map[int]struct{}{},
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Select environment variables:\n\n"
+
+	// Render the choices
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		checked := " "
+		if _, ok := m.selected[i]; ok {
+			checked = "x"
+		}
+
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
+
+	s += "\nPress q to quit."
+	return s
 }
