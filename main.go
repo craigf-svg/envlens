b79899ae6dcdef7daf9a1147500e1008f65e0a70
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-11-13 22:46:30 -0500

    add mode system with normal and search modes

--- main.go
+++ main.go
@@ -8,11 +8,17 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const (
+	modeNormal = "Normal"
+	modeSearch = "Search"
+)
+
 type model struct {
 	variables []string
 	cursor    int
 	choices   []string
 	selected  map[int]struct{}
+	mode      string
 }
 
 func main() {
@@ -26,7 +32,7 @@ func main() {
 		printList(envList)
 	}
 
-	p := tea.NewProgram(initialModel(envList))
+	p := tea.NewProgram(initialModel(envList, modeNormal))
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
@@ -40,11 +46,12 @@ func printList(list []string) {
 	fmt.Println("Total Number of Environment Variables", len(list))
 }
 
-func initialModel(prop []string) model {
+func initialModel(prop []string, initMode string) model {
 	return model{
 		variables: prop,
 		choices:   prop,
 		selected:  map[int]struct{}{},
+		mode:      initMode,
 	}
 }
 
@@ -59,33 +66,44 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	// Is it a key press?
 	case tea.KeyMsg:
 
-		// Cool, what was the actual key pressed?
-		switch msg.String() {
+		switch m.mode {
+		case modeNormal:
+			// Cool, what was the actual key pressed?
+			switch msg.String() {
 
-		// These keys should exit the program.
-		case "ctrl+c", "q":
-			return m, tea.Quit
+			// These keys should exit the program.
+			case "ctrl+c", "q":
+				return m, tea.Quit
 
-		// The "up" and "k" keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
+			// The "up" and "k" keys move the cursor up
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+
+			// The "down" and "j" keys move the cursor down
+			case "down", "j":
+				if m.cursor < len(m.choices)-1 {
+					m.cursor++
+				}
+
+			// The "enter" key and the spacebar (a literal space) toggle
+			// the selected state for the item that the cursor is pointing at.
+			case "enter", " ":
+				_, ok := m.selected[m.cursor]
+				if ok {
+					delete(m.selected, m.cursor)
+				} else {
+					m.selected[m.cursor] = struct{}{}
+				}
+
+			case "s":
+				m.mode = modeSearch
 			}
-
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
+		case modeSearch:
+			switch msg.String() {
+			case "esc":
+				m.mode = modeNormal
 			}
 		}
 	}
@@ -113,6 +131,14 @@ func (m model) View() string {
 		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
 	}
 
-	s += "\nPress q to quit."
+	s += "\nPress q to quit.\n"
+	switch m.mode {
+	case modeNormal:
+		s += "Normal mode - press s to search."
+	case modeSearch:
+		s += "Search mode - press esc to exit."
+	default:
+		s += "Unknown mode."
+	}
 	return s
 }
