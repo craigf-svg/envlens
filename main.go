cfe8fbe1f684276afd921b88c9633ebb93d255ce
craigf-svg <35483367+craigf-svg@users.noreply.github.com>
2025-11-21 10:18:11 -0500

    update detail mode

--- main.go
+++ main.go
@@ -15,13 +15,16 @@ const (
 )
 
 type model struct {
-	variables  []string
-	cursor     int
-	choices    []string
-	selected   map[int]struct{}
-	mode       string
-	searchTerm string
-	localVars  []string
+	variables     []string
+	cursor        int
+	choices       []string
+	selected      map[int]struct{}
+	mode          string
+	searchTerm    string
+	localVars     []string
+	localChoices  []string
+	localSelected map[int]struct{}
+	localCursor   int
 }
 
 func main() {
@@ -37,12 +40,17 @@ func main() {
 		fmt.Println("localEnv", localEnv)
 	}
 
+	var envSlice []string
+	for k, v := range localEnv {
+		envSlice = append(envSlice, k+"="+v)
+	}
+
 	var envList []string = os.Environ()
 	if os.Getenv("DEBUG") != "" {
 		printList(envList)
 	}
 
-	p := tea.NewProgram(initialModel(envList, modeNormal))
+	p := tea.NewProgram(initialModel(envList, modeNormal, envSlice))
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
@@ -56,13 +64,16 @@ func printList(list []string) {
 	fmt.Println("Total Number of Environment Variables", len(list))
 }
 
-func initialModel(prop []string, initMode string) model {
+func initialModel(envList []string, initMode string, localEnv []string) model {
 	return model{
-		variables:  prop,
-		choices:    prop,
-		selected:   map[int]struct{}{},
-		mode:       initMode,
-		searchTerm: "",
+		variables:     envList,
+		choices:       envList,
+		selected:      map[int]struct{}{},
+		mode:          initMode,
+		searchTerm:    "",
+		localVars:     localEnv,
+		localChoices:  localEnv,
+		localSelected: map[int]struct{}{},
 	}
 }
 
@@ -79,27 +90,21 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		switch m.mode {
 		case modeNormal:
-			// Cool, what was the actual key pressed?
 			switch msg.String() {
-
 			// These keys should exit the program.
 			case "ctrl+c", "q":
 				return m, tea.Quit
-
 			// The "up" and "k" keys move the cursor up
 			case "up", "k":
 				if m.cursor > 0 {
 					m.cursor--
 				}
-
 			// The "down" and "j" keys move the cursor down
 			case "down", "j":
 				if m.cursor < len(m.choices)-1 {
 					m.cursor++
 				}
 
-			// The "enter" key and the spacebar (a literal space) toggle
-			// the selected state for the item that the cursor is pointing at.
 			case "enter", " ":
 				_, ok := m.selected[m.cursor]
 				if ok {
@@ -125,6 +130,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			switch msg.String() {
 			case "esc":
 				m.mode = modeNormal
+			case "ctrl+c", "q":
+				return m, tea.Quit
+			case "up", "k":
+				if m.localCursor > 0 {
+					m.localCursor--
+				}
+			case "down", "j":
+				if m.localCursor < len(m.localChoices)-1 {
+					m.localCursor++
+				}
+			case "enter", " ":
+				_, ok := m.localSelected[m.localCursor]
+				if ok {
+					delete(m.localSelected, m.localCursor)
+				} else {
+					m.localSelected[m.localCursor] = struct{}{}
+				}
 			}
 		}
 	}
@@ -145,20 +167,39 @@ func renderList(model model) string {
 	var renderedList string
 	renderedList = ""
 
-	for index, choice := range model.choices {
-		cursor := " "
-		if model.cursor == index {
-			cursor = ">"
-		}
+	switch model.mode {
 
-		checked := " "
-		if _, ok := model.selected[index]; ok {
-			checked = "x"
-		}
+	case modeDetail:
+		for index, choice := range model.localVars {
+			localCursor := " "
+			if model.localCursor == index {
+				localCursor = ">"
+			}
 
-		renderedList += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+			checked := " "
+			if _, ok := model.localSelected[index]; ok {
+				checked = "x"
+			}
+
+			renderedList += fmt.Sprintf("%s [%s] %s\n", localCursor, checked, choice)
+		}
+		return renderedList
+	default:
+		for index, choice := range model.choices {
+			cursor := " "
+			if model.cursor == index {
+				cursor = ">"
+			}
+
+			checked := " "
+			if _, ok := model.selected[index]; ok {
+				checked = "x"
+			}
+
+			renderedList += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+		}
+		return renderedList
 	}
-	return renderedList
 }
 
 func renderFooter(m model) string {
